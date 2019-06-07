7bc839a2e21fd672bbc27b1d67d71816a910f519
Dima <gritsenko275@gmail.com>
2019-06-07 17:19:46 +0300

    topExchanges

--- src/pages/topExchanges/topExchanges.js
+++ src/pages/topExchanges/topExchanges.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import './topExchanges.css';
 
 const TopExchanges = (props)=>{
     return(
         <div>
             <h2>TopExchanges</h2>
+            <TopExchangesData/>
+        </div>
+    )
+}
+
+class TopExchangesData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading:true,
+            data:[]
+        };
+    }
+    componentDidMount() {
+        fetch('https://min-api.cryptocompare.com/data/top/exchanges?fsym=BTC&tsym=USD').then(data=>data.json()).then((data)=> {
+            console.log(data.Data);
+            this.setState({
+                isLoading:false,
+                data:data.Data
+            })
+        })
+    }
+    render () {
+        if(this.state.isLoading) {
+        return (
+            <div>
+                <h3>Loading...</h3>
+
+            </div>
+        )
+    }
+        else{
+            return (
+            <div>
+               {this.state.data.map(elem =><ExchangesCard exchange = {elem.exchange} fromSymbol = {elem.fromSymbol} toSymbol = {elem.toSymbol} volume = {elem.volume24hTo}/>)}    
+            </div>)
+    }
+
+}
+}
+
+function ExchangesCard(props) {
+    return (
+        <div className = "ExchangesCard">
+            <h3>{"Exchange: "+ props.exchange}</h3>
+            <h4>{"From Symbol: "+props.fromSymbol}</h4>
+            <h4>{"To Symbol: "+props.toSymbol}</h4>
+            <h4>{"volume: "+ props.volume}</h4>
         </div>
     )
 }
